4f1c87bff82aa782db393bfa123a6271289e4a01
Gustavo Cunha <gustavocunha42@gmail.com>
2024-12-09 21:30:53 -0300

    Criando novos arquivos php

---
+++ tipos.php
@@ -0,0 +1,17 @@
+<?php
+
+$idade = 25;
+
+$salario = 1000.30;
+
+$divisao = 10 / 3;
+
+$texto = "ola mundo";
+
+$verdadeiro = true;
+
+$falso = false;
+
+//tipos primitivos.
+
+echo gettype($salario);
