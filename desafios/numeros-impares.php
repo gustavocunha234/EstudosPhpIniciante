4a4ee48079f56e02ec2016b9d5554ce3f5c2697f
Gustavo Cunha <gustavocunha42@gmail.com>
2024-12-11 22:46:29 -0300

    desafios de numeros-impares e tabuada de 1 a 9

---
+++ desafios/numeros-impares.php
@@ -0,0 +1,11 @@
+<?php 
+
+for ($i = 0; $i <= 100; $i++) {
+    if($i % 2 == 0){
+        echo "Numeros pares $i" . PHP_EOL;
+    }
+    else{
+        echo "Numeros impares $i" . PHP_EOL;
+    }
+
+}
---
+++ desafios/tabuada.php
@@ -0,0 +1,11 @@
+<?php 
+
+$num = 9;
+$result = 0;
+
+for ($i = 0; $i < $num; $i++) {
+   
+     $result = $num * $i;
+
+     echo "$num X $i" . " = " . "$result " . PHP_EOL;
+}
